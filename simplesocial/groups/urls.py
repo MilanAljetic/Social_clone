804c85a56711f505f465a544290cf6cb4c4e8340
MilanAljetic <aljetic23@gmail.com>
2020-04-18 23:53:42 +0200

    Social_clone2

--- simplesocial/groups/urls.py
+++ simplesocial/groups/urls.py
@@ -6,6 +6,7 @@ app_name = 'groups'
 urlpatterns = [
     path('',views.ListGroup.as_view(),name='all'),
     path('new/',views.CreateGroup.as_view(),name='create'),
-    path('posts/in/(?P<slug>[-\w]+)',views.SingleGroup.as_view(),name='single')
-
+    path('posts/in/(?P<slug>[-\w]+)',views.SingleGroup.as_view(),name='single'),
+    path('join/(?P<slug>[-\w]+)',views.JoinGoup.as_view(),name='join'),
+    path('leave/(?P<slug>[-\w]+)',views.LeaveGroup.as_view(),name='leave'),
 ]
